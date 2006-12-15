a0cc64ae5682d040131938d261b59de6d48cb50e
Bill Welliver <bill@welliver.org>
2006-12-15 03:10:30 +0000

    ok, first stab at an iterator
    --HG--
    extra : convert_revision : svn%3A2b5a5965-8a46-0410-aa19-a76fe43533fc/trunk%402565

--- OC_Mapping.m
+++ OC_Mapping.m
@@ -3,9 +3,10 @@
 
 @interface OC_MappingEnumerator  : NSEnumerator
 {
-	struct mapping* mapping;
+	struct object* iterator;
 	BOOL valid;
-	int pos;
+	int next_id;
+	int value_id;
 }
 + newWithWrappedMapping:(OC_Mapping*)value;
 - initWithWrappedMapping:(OC_Mapping*)value;
@@ -24,14 +25,34 @@
 
 - initWithWrappedMapping:(OC_Mapping*)value
 {
+	struct mapping * m;
+	
 	self = [super init];
 	if (!self) return nil;
 
-	mapping = [v retain];
+    m = [value __ObjCgetPikeMapping];
+
+	// do we need to do this?
+    ref_push_mapping(m);
+    f_get_iterator(1);
+    
+    if(Pike_sp[-1].type != T_OBJECT)
+	{
+		NSException *exception = [NSException exceptionWithName:@"IteratorException"
+		                            reason:@"Unable to get an iterator for the mapping."  userInfo:nil];
+		@throw exception;
+	}
+
+	iterator = Pike_sp[-1].u.object;
+
+    add_ref(iterator);
+    pop_stack();
+
+	next_id=find_identifier("next", iterator->program);
+	next_id=find_identifier("value", iterator->program);
+
 	valid = YES;
-	pos = 0;
 	return self;
-	
 }
 
 -(void)dealloc
@@ -40,9 +61,41 @@
 	[super dealloc];	
 }
 
+
+// TODO: do we need to do this the other way around? We might be missing the first object.
 -(id)nextObject
 {
+	id rv;
 	
+    printf("next object\n");
+	apply_low(iterator, next_id, 0);
+	
+	if(Pike_sp[-1].type != T_INT)
+	{
+		NSException *exception = [NSException exceptionWithName:@"IteratorException"
+		                            reason:@"Something went wrong while advancing the iterator."  userInfo:nil];
+		@throw exception;		
+	}
+	
+	if(!Pike_sp[-1].u.integer)
+	{
+		pop_stack();
+		return nil;
+	}	
+	else
+	{
+		apply_low(iterator, value_id, 0);
+		if(Pike_sp[-1].type == T_INT && Pike_sp[-1].subtype)
+		{
+			pop_n_elems(2);
+			return nil;
+		}
+		
+		rv = svalue_to_id(Pike_sp[-1]);
+		pop_n_elems(2);
+		
+		return rv;
+	}
 }
 
 @end /* implementation OC_MappingEnumerator */
